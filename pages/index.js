c3fb1212d73bc2d3d03740115e32707b9fa49e96
Smit jogani <100015904+smitjogani@users.noreply.github.com>
2023-08-11 12:17:37 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -64,7 +64,7 @@ export default function Home() {
             <h2 className="text-5xl py-2 text-blue-500 font-medium md:text-6xl">
               Smit Jogani
             </h2>
-            <h3 className="txt-2xl py-2">Software Devloper</h3>
+            <h3 className="txt-2xl py-2">Software Developer</h3>
             <p className="text-md py-5 leading-8 text-gray-800 dark:text-slate-400">
               Freelancer providing services for programming and design content
               needs. Join me down below and let's get cracking!
